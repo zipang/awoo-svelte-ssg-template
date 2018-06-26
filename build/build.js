4eb0f89476c95b5378e5ecf588572baf99f09d33
zipang <christophe.desguez@gmail.com>
2018-06-26 13:06:15 +0200

    Switched back to site.use() for a chainable and compatible syntax to add plugins (and options)

--- build/build.js
+++ build/build.js
@@ -4,42 +4,16 @@ var path = require("path"),
 	matter = require("awoo-matter"),
 	svelte = require("./plugins/awoo-svelte-plugin");
 
-matter.name = "awoo-matter";
-
-async function buildLatePlugin() {
-
-	function latePlugin(files) {
-		console.log(`I'm the late plugin running now on ${files.length} files`);
-		throw new Error(`Oh no latePlugin() failed !`);
-		return files;
-	}
-
-	throw new Error(`oh no could not init latePlugin`);
-	return latePlugin;
-	return new Promise(resolve => {
-		setTimeout(() => resolve(latePlugin), 2000);
-	});
-}
-
-awoo(site => {
-	site.config({
+awoo.build(site => site
+	.use({
+		title: "My beautiful site",
+		lang: "en",
 		source: path.join(__dirname, "../content/"),
 		destination: path.join(__dirname, "../public/")
 	})
-	site.use(matter)
-	site.use(buildLatePlugin)
-	return site
-})
-
-// awoo(site => site
-// 	.add({
-// 		source: path.join(__dirname, "../content/"),
-// 		destination: path.join(__dirname, "../public/")
-// 	})
-// 	.add(files => files.filter(file => file.extname === '.md'))
-// 	.add(matter)
-// 	//.add(buildLatePlugin)
-// 	.add(svelte, {
-// 		layoutsDir: path.join(__dirname, "../theme/layouts/")
-// 	})
-// );
+	.use(files => files.filter(file => file.extname === '.md'))
+	.use(matter)
+	.use(svelte, {
+		layoutsDir: path.join(__dirname, "../theme/layouts/")
+	})
+);
